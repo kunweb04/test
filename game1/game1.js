4b278e7fc878da631c4d8c50034739f824393a09
kunweb04 <enderwolf9487@gmail.com>
2024-10-29 19:04:40 +0800

    Add files via upload

--- game1/game1.js
+++ game1/game1.js
@@ -1,8 +1,7 @@
 const questions = [
     { question: "我是谁？", answer: "oho" },
     { question: "我的生日？", answer: "4月17日" },
-    { question: "我是什么网页工程师？", answer: "前段网页工程师" },
-    // 更多问题...
+    { question: "我是什么类型的网页工程师？", answer: "前段网页工程师" },
 ];
 
 let currentQuestionIndex = 0;
